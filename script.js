4af3f844ed702648efa4d61db3aadaa490be77a6
Hamidreza Jafari <87299262+Hamidrezaj@users.noreply.github.com>
2025-01-24 21:43:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// part 1
+
+let studentName = 'Hamid Jafari';
+let studentAge = 31;
+let isEnrolled = false;
+
+console.log(studentName);
+console.log(studentAge);
+console.log(isEnrolled);
+
+// part 2
+
+let title = 'Being';
+let magicalNumber = 0;
+let infiniteWorld = true;
+let nonOpposite = ['peace', 'joy', 'love'];
+let theWorld = {
+  form: 'delusion',
+  loc: 'earth',
+  age: 13.4,
+  orient: 0,
+};
+
+console.log(title);
+console.log(magicalNumber);
+console.log(infiniteWorld);
+console.log(nonOpposite);
+console.log(theWorld);
+
+// part 3
+
+console.log(studentAge + theWorld.age);
+console.log(theWorld.age - studentAge);
+console.log(studentAge / theWorld.age);
+console.log(studentAge * magicalNumber);
+
+console.log(studentAge > magicalNumber);
+console.log(studentAge < theWorld.orient);
+console.log(magicalNumber === theWorld.orient);
+console.log(theWorld.orient !== theWorld.age);
+
+console.log(isEnrolled && infiniteWorld);
+console.log(isEnrolled || infiniteWorld);
+console.log(!isEnrolled);
